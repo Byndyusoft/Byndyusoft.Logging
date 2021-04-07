60f6f226b2c2df88f0e36633dbd4e1d5318c35ca
rusakov <rusakov@byndyusoft.com>
2021-04-07 15:43:35 +0500

    Перенёс LoggerConfigurationExtensions и всё с ним связанное

---
+++ src/Byndyusoft.Logging/Configuration/LoggerConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Byndyusoft.Logging.Enrichers;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace Byndyusoft.Logging.Configuration
+{
+    public static class LoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration UseDefaultSettings(
+            this LoggerConfiguration loggerConfiguration,
+            IConfiguration configuration)
+        {
+            if (loggerConfiguration == null)
+                throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            return loggerConfiguration
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithServiceName(Environment.GetEnvironmentVariable("SERVICE_NAME"))
+                .Enrich.WithApplicationInformationalVersion()
+                
+                .Enrich.WithMessageTemplateHash()
+                .Enrich.WithLogEventHash()
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(configuration);
+        }
+    }
+}
