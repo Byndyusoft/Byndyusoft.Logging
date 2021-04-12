74377105eb86b52a7d577ffdee8e10318cf6af44
rusakov <rusakov@byndyusoft.com>
2021-04-12 11:37:39 +0500

    Убрал ParentId из логов

--- src/Byndyusoft.Logging/Enrichers/OpenTracingTraceEnricher.cs
+++ src/Byndyusoft.Logging/Enrichers/OpenTracingTraceEnricher.cs
@@ -17,6 +17,7 @@ namespace Byndyusoft.Logging.Enrichers
             }
             logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("TraceId", activeSpan.Context.TraceId));
             logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("SpanId", activeSpan.Context.SpanId));
+            logEvent.RemovePropertyIfPresent("ParentId");
         }
     }
 }
