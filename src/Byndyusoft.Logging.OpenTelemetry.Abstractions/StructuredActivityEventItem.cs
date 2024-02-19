64dc0a0e2f4fc8a52ef3128b313649e1be10de9d
rgaynetdinov <foxcoolman@byndyusoft.com>
2024-02-19 13:11:46 +0500

    Комментарии и форматирование

--- src/Byndyusoft.Logging.OpenTelemetry.Abstractions/StructuredActivityEventItem.cs
+++ src/Byndyusoft.Logging.OpenTelemetry.Abstractions/StructuredActivityEventItem.cs
@@ -1,18 +1,27 @@
 ﻿namespace Byndyusoft.Logging
 {
+    /// <summary>
+    ///     Параметр структурного лога
+    /// </summary>
     public class StructuredActivityEventItem
     {
-        public string Name { get; }
-
-        public object Value { get; }
-
-        public string Description { get; }
-
         public StructuredActivityEventItem(string name, object value, string description)
         {
             Name = name;
             Value = value;
             Description = description;
         }
+
+        /// <summary>
+        ///     Имя параметра
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///     Значение параметра
+        /// </summary>
+        public object Value { get; }
+
+        public string Description { get; }
     }
 }
